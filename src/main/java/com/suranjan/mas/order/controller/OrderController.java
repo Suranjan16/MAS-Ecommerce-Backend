96f00345f2c92facece7a62ca4c2ab9240e7522f
Suranjan16 <suranjanmaity851@gmail.com>
2026-05-20 20:45:20 +0530

    feat: add OrderController and implement place order API

---
+++ src/main/java/com/suranjan/mas/order/controller/OrderController.java
@@ -0,0 +1,29 @@
+package com.suranjan.mas.order.controller;
+
+import com.suranjan.mas.auth.entity.User;
+import com.suranjan.mas.auth.repository.UserRepository;
+import com.suranjan.mas.order.service.OrderService;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+    private final OrderService orderService;
+    private final UserRepository userRepository;
+
+    public OrderController(OrderService orderService, UserRepository userRepository) {
+        this.orderService = orderService;
+        this.userRepository = userRepository;
+    }
+
+    @PostMapping("/place")
+    public String placeOrder(Authentication authentication) {
+        String email = authentication.getName();
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        return orderService.placeOrder(user);
+    }
+}
