5ebd022858ea5cd7f8814a102da1a6028e582182
Suranjan16 <suranjanmaity851@gmail.com>
2026-05-20 19:44:22 +0530

    feat: add OrderItem entity for order product tracking

---
+++ src/main/java/com/suranjan/mas/order/entity/OrderItem.java
@@ -0,0 +1,57 @@
+package com.suranjan.mas.order.entity;
+
+
+import com.suranjan.mas.product.entity.Product;
+import jakarta.persistence.*;
+
+@Entity
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Order order;
+
+    @ManyToOne
+    private Product product;
+
+    private Integer quantity;
+    private Double price;
+
+    public Long getId() {
+        return id;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
