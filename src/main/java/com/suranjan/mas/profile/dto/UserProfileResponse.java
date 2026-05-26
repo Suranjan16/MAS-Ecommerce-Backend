9297fc1f1c5e63143743291b9576a638981133c9
Suranjan16 <suranjanmaity851@gmail.com>
2026-05-26 12:53:57 +0530

    feat: add user profile request and response DTOs

---
+++ src/main/java/com/suranjan/mas/profile/dto/UserProfileResponse.java
@@ -0,0 +1,43 @@
+package com.suranjan.mas.profile.dto;
+
+import java.time.LocalDate;
+
+public class UserProfileResponse {
+
+    private String name;
+    private LocalDate dob;
+    private String gender;
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
